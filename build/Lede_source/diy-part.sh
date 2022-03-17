5fe650cd53fbb54357e9ff908089e4b9a754d712
yufengqinli <62143471+yufengqinli@users.noreply.github.com>
2022-03-17 14:57:17 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -5,26 +5,26 @@
 #
 
 cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='10.10.10.251'                                   # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='10.10.10.51'                                   # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='10.10.10.250'                                  # IPv4 网关
-uci set network.lan.broadcast='10.10.10.255'                                # IPv4 广播
-uci set network.lan.dns='10.10.10.253'                                      # DNS(多个DNS要用空格分开)
+uci set network.lan.gateway='10.10.10.1'                                  # IPv4 网关
+#uci set network.lan.broadcast='10.10.10.255'                                # IPv4 广播
+uci set network.lan.dns='10.10.10.1'                                      # DNS(多个DNS要用空格分开)
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
 uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='OpenWrt'                                # 修改主机名称为OpenWrt
+uci set system.@system[0].hostname='Yufeng'                                # 修改主机名称为Yufeng
 EOF
 
-# 版本号里显示一个自己的名字（MCydia build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+# 版本号里显示一个自己的名字（Yufeng build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
 
 # 关闭IPv6 分配长度
 sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
                                                 
-# 选择opentomcat为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' feeds/luci/collections/luci/Makefile                    
+# 选择Bootstrap为默认主题
+sed -i 's/luci-theme-Bootstrap/luci-theme-opentomcat/g' feeds/luci/collections/luci/Makefile                    
 
 # 替换密码（要替换密码就不能设置密码为空）
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$PhflQnJ1$yamWfH5Mphs4hXV7UXWQ21:18725/g' $ZZZ          
